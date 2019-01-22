d3dbb25c7fd5cd83edb6066dbfbbf5d784d840d1
Dim Samoilenko <-m>
2019-01-23 01:49:06 +0200

    First commit

---
+++ Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBotApp.Models
+{
+    public class AppSettings
+    {
+        public static string Url { get; set; } = "https://bpt-work.herokuapp.com:443{0}";
+
+        public static string Name { get; set; } = "extremecode_bot";
+
+        public static string Key { get; set; } = "650722338:AAGDMVx9cXMLmxqt63BWD6GZSiXmuNlBrbo";
+
+    }
+}
---
+++ Models/Commands/HelloCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBotApp.Models.Commands
+{
+    public class HelloCommand : Command
+    {
+        public override string Name => "Hello";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+
+            //TODO Logic -_-
+            await client.SendTextMessageAsync(0, "", replyToMessageId: 0);
+        }
+    }
+}
